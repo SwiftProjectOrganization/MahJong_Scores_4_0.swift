4bbd2b547857e3835bb6e5f245cb5b3e5e7e410f
Rob J Goedman <goedman@icloud.com>
2024-05-28 10:28:17 -0600

    Updated title display formatting ( in TournamentTitleView) and colors in IndividualTournamentView

---
+++ MahJong_Scores_2_0/Views/Tournament Views/TournamentTitleView.swift
@@ -0,0 +1,18 @@
+//
+//  TournamentTitleView.swift
+//  MahJong_Scores_2_0
+//
+//  Created by Robert Goedman on 5/28/24.
+//
+
+import SwiftUI
+
+struct TournamentTitleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TournamentTitleView()
+}
